c0ca92fd89eb90a26093de73945ec31e44ba8e2b
Ambuj <ambujs.app@gmail.com>
2014-10-07 21:40:36 -0700

    Add nginx config files for api server

--- server/setup.py
+++ server/setup.py
@@ -1,8 +1,13 @@
 from distutils.core import setup
 
-setup(name="OSOM_server",
+setup(name="osom-server",
       version="0.1",
       description="One Serving of Motivation: Server",
       author="Ambuj Sinha",
       author_email="ambujs.app@gmail.com",
-      packages=['OSOM_server'])
+      packages=['OSOM_server'],
+      data_files=[
+        ('/etc/nginx/conf.d/nginx-osom/',['nginx/conf.d/nginx-osom/nginx-osom-api.conf']),
+        ('/etc/nginx/sites-enabled/',['nginx/sites-enabled/default']),
+      ]
+     )
