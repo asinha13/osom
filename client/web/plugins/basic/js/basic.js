4eb87a0b3d17f31bebc8b376fee49b55eb9a6010
Ambuj <ambujs.app@gmail.com>
2014-10-07 21:51:15 -0700

    Add basic web plugin

---
+++ client/web/plugins/basic/js/basic.js
@@ -0,0 +1,59 @@
+var module = angular.module('OSOMBasicApp',[])
+
+module.factory('svcBasic',["$http","$timeout",function($http, $timeout) {
+    var self = {};
+    var refresh_rate = 0; // Never refresh
+    self.osom = {};
+    var retries = 0;
+    var max_retries = 3;
+
+    var _poller = function() {
+        self.get_new_osom()
+        if (refresh_rate > 0) {
+            $timeout(function to () {
+                _poller();
+            },refresh_rate*1000);
+        }
+    };
+
+    self.set_refersh_rate = function(rate) {
+        var old_rate = refresh_rate;
+        refresh_rate = rate;
+        if (old_rate<=0 && rate > 0) {
+            _poller();
+        }
+    };
+
+    self.get_osom_object = function() {
+        self.get_new_osom();
+        return self.osom;
+    };
+
+    self.get_new_osom = function() {
+        $http.get('/api/')
+            .success(function(data,status) {
+                retries = 0;
+                if (status!=200) {
+                    console.log("Request status : ",status);
+                    return;
+                }
+                angular.copy(data,self.osom);
+            })
+            .error(function(data,status) {
+                if (retries <= max_retries) {
+                    retries -= 1;
+                    self.get_new_osom();
+                }
+            });
+    };
+    return self;
+}]);
+
+
+module.controller('ctrlBasic', ["$scope","svcBasic",function($scope, svcBasic) {
+    $scope.osom = svcBasic.get_osom_object();
+
+    $scope.set_refresh_rate = function(rate) {
+        svcBasic.set_refresh_rate();
+    };
+}]);
